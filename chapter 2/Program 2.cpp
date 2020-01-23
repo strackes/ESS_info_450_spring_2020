918286628d97dd9fbf30ecff2aea1ab0f6c85362
strackes <59985848+strackes@users.noreply.github.com>
2020-01-22 20:03:46 -0500

    Update Program 2.cpp

--- chapter 2/Program 2.cpp
+++ chapter 2/Program 2.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main()
 {
-  int x;
+  int x;  //this declares a variable
   
-  x=1023;
+  x=1023;  //this assigns 1023 to x
   
   cout << "This program prints the value of x: ";
-  cout << x;
+  cout << x;  //This displays 1023
   
   return 0;
 }
