a55f8ff8329e9806642922757e93b40b18ca23cc
strackes <59985848+strackes@users.noreply.github.com>
2020-01-22 19:57:43 -0500

    Create Program 3.cpp

---
+++ chapter 2/Program 3.cpp
@@ -0,0 +1,19 @@
+//This program converts gallons to liters.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int gallons;
+  int liters;
+  
+  cout << "Enter number of gallons: ";
+  cin >> gallons; //this inputs from the user
+  
+  liters = gallons * 4; // convert to liters
+  
+  cout << "Liters: " << liters;
+  
+  return 0;
+}
