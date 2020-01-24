d2624a574535f540481baa5cc97912ea937f9786
strackes <59985848+strackes@users.noreply.github.com>
2020-01-23 19:13:07 -0500

    Create program4.cpp

---
+++ chapter2/program4.cpp
@@ -0,0 +1,19 @@
+/*This program converts gallons to liters using floating point numbers. */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  double gallons;
+  double liters;
+  
+  cout << "Enter number of gallons: ";
+  cin >> gallons; // this inputs from the user
+  
+  liters = gallons * 3.7854; //convert to liters
+  
+  cout << "Liters: " << liters;
+  
+  return 0;
+}
