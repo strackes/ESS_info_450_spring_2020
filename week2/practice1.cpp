5d16b268e99e18b076349fc7d40cfb8921c40e87
strackes <59985848+strackes@users.noreply.github.com>
2020-01-30 14:02:15 -0500

    Create practice1.cpp

---
+++ week2/practice1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   
+    int value;
+    cout << "Please enter a postive number: " << endl;
+    cin >> value;
+    cout << "Your value squared is: " << pow(value, 2) << endl;
+   
+    
+    return 0;
+ 
+}
