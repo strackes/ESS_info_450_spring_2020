e0e46e59c428a8963e6f9ad8dd5a3a53f89d0c56
strackes <59985848+strackes@users.noreply.github.com>
2020-02-04 21:24:26 -0500

    Create circumference.cpp

---
+++ week3/circumference.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+const double PI = 3.14;
+
+int main(){
+
+double radius;
+double ComputeCircumference;
+     
+void getCirumference(double ComputeCircumference(double radius, double c, double PI));
+
+cout <<" What is the radius?" << (ComputeCircumference = 2 * PI * radius) << endl;
+
+
+
+
+return 0;
+
+}
+
+  
